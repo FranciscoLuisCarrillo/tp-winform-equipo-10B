c6f5d268c4765245d7a2577fd4cd23563d0a50fb
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-09-10 19:37:17 -0300

    C7 - Generacion de atributos de Marca
    Actualizo atributos de dominio --> marca

--- dominio/Marca.cs
+++ dominio/Marca.cs
@@ -8,5 +8,12 @@ namespace dominio
 {
     public class Marca
     {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public string ToString()
+        {
+            return Descripcion;
+        }
     }
 }
