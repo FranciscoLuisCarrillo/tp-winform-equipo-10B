dfda1fbd77de4f0cb158065d834c7f3edad374ee
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-09-14 15:05:35 -0300

    C - C36 Agrego Listar Categoria

--- negocio/CategoriaNegocio.cs
+++ negocio/CategoriaNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,36 @@ namespace negocio
 {
     public class CategoriaNegocio
     {
+
+        public List<Categoria> listar()
+            {
+            List<Categoria> lista = new List<Categoria>();
+            Acceso datos = new Acceso();
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion from CATEGORIAS");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+
+
+
+        }
     }
 }
